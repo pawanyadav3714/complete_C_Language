8190dcb44c1307f07d1610a91944e8135a9f5768
Pawan Yadav <pawanyadav3714@gmail.com>
2026-07-27 11:31:12 +0530

    Delete Lec3 directory

--- Lec3/count.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    for(int i=0; i<=100; i+=2){ // i+=2 ==> i=i+2 ==> i=1+2=>3
-        printf("%d\n", i);
-    }
-    return 0;
-}
--- Lec3/table4.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    for(int i=5; i<=50; i+=5){  // 4,8,12,...40
-        printf("%d, ",i);
-    }
-    return 0;
-}
